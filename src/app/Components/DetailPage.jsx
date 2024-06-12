a8be1a0428c1bd85ea497774f1c7ea2a17915e47
Alif Ahmed <alifahmed102@gmail.com>
2024-06-12 16:41:35 +0600

    'ogimage'

--- src/app/Components/DetailPage.jsx
+++ src/app/Components/DetailPage.jsx
@@ -2,28 +2,31 @@
 import React from 'react';
 import PostBody from './Shared/Postbody';
 import Image from 'next/image';
-
-
-
+import Head from 'next/head';
 
 const DetailPage = ({ post }) => {
   const imageUrl = `https://admin.desh365.top/public/storage/post-image/${post?.image}`;
-  const currentUrl = 'https://news-nextjs-phi.vercel.app'
-  
+  const currentUrl = typeof window !== 'undefined' ? window.location.href : 'https://news-nextjs-phi.vercel.app';
 
   if (!post) {
     return <div>Loading...</div>;
   }
-  console.log(post)
-
+  console.log(post);
 
   return (
     <div>
-     
+      <Head>
+        <title>{post?.title}</title>
+        <meta property="og:title" content={post?.title} />
+        <meta property="og:description" content={post?.post_body?.substring(0, 150)} />
+        <meta property="og:image" content={imageUrl} />
+        <meta property="og:url" content={currentUrl} />
+        <meta property="og:type" content="article" />
+      </Head>
 
-      <div className='p-2 space-y-5' >
+      <div className='p-2 space-y-5'>
         <a
-          href={`https://www.facebook.com/sharer/sharer.php?u=${(currentUrl)}`}
+          href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(currentUrl)}`}
           target='_blank'
           rel='noopener noreferrer'
         >
@@ -32,9 +35,7 @@ const DetailPage = ({ post }) => {
           </button>
         </a>
 
-
-        <h1 className='f text-[22px]  font-bold'> {post?.title} </h1>
-
+        <h1 className='f text-[22px] font-bold'>{post?.title}</h1>
 
         <div className='rounded-md overflow-hidden relative' style={{ height: '360px', width: '100%' }}>
           <Image
@@ -46,39 +47,9 @@ const DetailPage = ({ post }) => {
         </div>
 
         <PostBody postBody={post?.post_body} />
-
       </div>
-
-
-
-
     </div>
-  )
+  );
 }
 
 export default DetailPage;
-
-
-{/* <Head>
-        <title>{post?.title}</title>
-        <meta property="og:title" content={post?.title} />
-        <meta property="og:description" content={post?.Category_name} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:url" content={window.location.href} />
-      </Head> */}
-{/* 
-      <div className='flex flex-row gap-5 justify-center mt-7'>
-        <a
-          href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <button className='b bg-blue-600 p-2 text-white rounded-lg'>
-            Share on Facebook
-          </button>
-        </a>
-        <Link href='/'>
-          <button className='b bg-purple-300 p-2 rounded-lg'>Go Home</button>
-        </Link>
-      </div> */}
-
