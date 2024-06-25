3ab77fb64b1d0aac46e6a476469914501d349dc4
Alif Ahmed <alifahmed102@gmail.com>
2024-06-25 16:55:11 +0600

    'pryertimee'

--- src/app/Components/IndexCom/PrayerTimePage.jsx
+++ src/app/Components/IndexCom/PrayerTimePage.jsx
@@ -8,22 +8,24 @@ async function getPrayerTimes() {
 
 const PrayerTimesPage = async () => {
   const prayerTimes = await getPrayerTimes();
+  const prayerTimesArray = Object.keys(prayerTimes).slice(0, 7);
+
+  // Filter out the 3rd element (index 2)
+  const filteredPrayerTimes = prayerTimesArray.filter((_, index) => index !== 4);
 
   return (
-    // <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    
       <ul className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
-      <h1 className="text-md px-3 font-bold mb-6">Prayer Times for Bangladesh</h1>
+      <h1 className="text-md px-3 font-bold mb-6">Prayer Times for Dhaka, Bangladesh</h1>
 
-        {Object.keys(prayerTimes).slice(0, 7).map((time) => (
-          
+        {filteredPrayerTimes.map((time) => (
           <li key={time} className="border-b last:border-b-0 py-2 flex justify-between">
-            
             <span className="font-medium">{time}</span>
             <span>{prayerTimes[time]}</span>
           </li>
         ))}
       </ul>
-    // </div>
+ 
   );
 };
 
