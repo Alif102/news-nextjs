1652ec16044037550fd1ed77572b5802fa34bbcc
Alif Ahmed <alifahmed102@gmail.com>
2024-07-04 15:42:00 +0600

    dynmicpage

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 // next.config.js
 module.exports = {
   // Specify the output configuration for static site generation
-  output: {
-    page: 'out',
-  },
+  output: 'out',
   images: {
     domains: ['admin.desh365.top', 'api.aladhan.com', 'news-nextjs-phi.vercel.app', 'newsportalnextjs.vercel.app'],
     loader: 'default',
