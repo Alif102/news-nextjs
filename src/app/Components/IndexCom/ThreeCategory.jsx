e64d5692f21319d48ff3e73f8ba1baa77756d0f0
Alif Ahmed <alifahmed102@gmail.com>
2024-07-09 12:26:56 +0600

    sommeechnge

--- src/app/Components/IndexCom/ThreeCategory.jsx
+++ src/app/Components/IndexCom/ThreeCategory.jsx
@@ -60,7 +60,7 @@ const ThreeCategories = ({ categoryNumber }) => {
 
 const ThreeCategory = () => {
   return (
-    <div className='grid grid-cols-3 py-2 gap-4 mt-10'>
+    <div className='grid grid-cols-3 py-4 gap-4 mt-10'>
       <ThreeCategories categoryNumber={1} />
       <ThreeCategories categoryNumber={2} />
       <ThreeCategories categoryNumber={3} />
